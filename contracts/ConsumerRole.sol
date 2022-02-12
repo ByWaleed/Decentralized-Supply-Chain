3b6f64ee0ef5245d64347ec1989043c8a53c7670
ByWaleed <waleedi825@gmail.com>
2022-02-12 22:06:36 +0000

    Method implemented: isConsumer, onlyConsumer, addConsumer, renounceConsumer

--- contracts/ConsumerRole.sol
+++ contracts/ConsumerRole.sol
@@ -32,4 +32,28 @@ contract ConsumerRole {
 
         emit ConsumerRemoved(account);
     }
+
+    // Define a function 'isConsumer' to check this role
+    function isConsumer(address account) public view returns (bool) {
+        return consumers.has(account);
+    }
+
+    // Define a modifier that checks to see if msg.sender has the appropriate role
+    modifier onlyConsumer() {
+        require(
+            isConsumer(msg.sender),
+            "Sender doesn't have the consumer role."
+        );
+        _;
+    }
+
+    // Define a function 'addConsumer' that adds this role
+    function addConsumer(address account) public onlyConsumer {
+        _addConsumer(account);
+    }
+
+    // Define a function 'renounceConsumer' to renounce this role
+    function renounceConsumer() public {
+        _removeConsumer(msg.sender);
+    }
 }
