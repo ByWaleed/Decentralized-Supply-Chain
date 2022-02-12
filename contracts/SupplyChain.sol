67da9640a3431362d3ff873ca4dd29b63e2699d4
ByWaleed <waleedi825@gmail.com>
2022-02-12 22:03:12 +0000

    ConsumerRole contract create with Add and Remove events

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -1,14 +1,4 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract SupplyChain {
-    // Automatically has getter and setter created
-    mapping(uint => int) public map;
-
-    function setKey(uint key, int value) public {
-        map[key] = value;
-    }
-
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
-    }
 }
