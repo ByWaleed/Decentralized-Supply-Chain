823bddb55488273b790d1ea6821506e5cf2ad590
ByWaleed <waleedi825@gmail.com>
2022-02-12 21:20:05 +0000

    ManufactuereRole contract file created

---
+++ contracts/ManufactuereRole.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+// Import the library 'Roles'
+import "./Roles.sol";
+
+// Define a contract 'ManufactuereRole' to manage this role - add, remove, check
+contract ManufactuereRole {
+
+}
