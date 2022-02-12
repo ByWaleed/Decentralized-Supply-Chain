cdebca3cfbdd9816214eac77531f819a7da63191
ByWaleed <waleedi825@gmail.com>
2022-02-12 21:29:48 +0000

    Typo fixes

--- contracts/ManufactuereRole.sol
+++ contracts/ManufactuereRole.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.4.22 <0.9.0;
 // Import the library 'Roles'
 import "./Roles.sol";
 
-// Define a contract 'ManufactuereRole' to manage this role - add, remove, check
-contract ManufactuereRole {
+// Define a contract 'ManufacturerRole' to manage this role - add, remove, check
+contract ManufacturerRole {
     using Roles for Roles.Role;
 
     // Define 2 events, one for Adding, and other for Removing
@@ -14,8 +14,13 @@ contract ManufactuereRole {
     // Define a struct 'manufacturers' by inheriting from 'Roles' library, struct Role
     Roles.Role private manufacturers;
 
-    // Define an internal function '_addFarmer' to add this role, called by 'addFarmer'
-    function _addManufactuere(address account) internal {
+    // In the constructor make the address that deploys this contract the 1st manufacturer
+    constructor() {
+        _addManufacturer(msg.sender);
+    }
+
+    // Define an internal function '_addManufactuere' to add this role, called by 'addManufacturer'
+    function _addManufacturer(address account) internal {
         manufacturers.add(account);
         emit ManufacturerAdded(account);
     }
