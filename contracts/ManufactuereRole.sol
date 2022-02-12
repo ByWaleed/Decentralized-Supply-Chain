7f068d5ab1c06ac38416fa3e7e0a9e75f5782a45
ByWaleed <waleedi825@gmail.com>
2022-02-12 21:23:22 +0000

    Manufactueres roles struct and the add & remove method created

--- contracts/ManufactuereRole.sol
+++ contracts/ManufactuereRole.sol
@@ -5,5 +5,12 @@ import "./Roles.sol";
 
 // Define a contract 'ManufactuereRole' to manage this role - add, remove, check
 contract ManufactuereRole {
+    using Roles for Roles.Role;
 
+    // Define 2 events, one for Adding, and other for Removing
+    event ManufacturerAdded(address indexed account);
+    event ManufacturerRemoved(address indexed account);
+
+    // Define a struct 'manufacturers' by inheriting from 'Roles' library, struct Role
+    Roles.Role private manufacturers;
 }
