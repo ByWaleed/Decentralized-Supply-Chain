67b5d745cdf2c5bcaeca7d0c5639ac23e438cf86
ByWaleed <waleedi825@gmail.com>
2022-02-15 20:17:08 +0000

    Supply Chain inherited contracts & created contract states

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -1,4 +1,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract SupplyChain {
+import "./Ownable.sol";
+import "./roles/Consumer.sol";
+import "./roles/Distributor.sol";
+import "./roles/Manufacturer.sol";
+import "./roles/Retailer.sol";
+
+contract SupplyChain is Ownable, Consumer, Distributor, Manufacturer, Retailer {
+    // Define a variable called 'upc' for Universal Product Code (UPC)
+    uint256 upc;
+
+    // Define enum 'State' with the following values (from 0 to 6):
+    enum State {
+        Manufactured,
+        Packed,
+        ForSale,
+        Sold,
+        Shipped,
+        Received,
+        Purchased
+    }
+
+    State constant defaultState = State.Manufactured;
 }
