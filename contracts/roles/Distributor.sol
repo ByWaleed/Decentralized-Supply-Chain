26250dadc75a811da0daa5bd6b4938de959e8b90
ByWaleed <waleedi825@gmail.com>
2022-02-15 19:27:12 +0000

    Roles contracts moved into roles directory

--- contracts/roles/Distributor.sol
+++ contracts/roles/Distributor.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 // Import the library 'Roles'
-import "./Roles.sol";
+import "./Roles.sol";w
 
 // Define a contract 'DistributorRole' to manage this role - add, remove, check
 contract DistributorRole {
