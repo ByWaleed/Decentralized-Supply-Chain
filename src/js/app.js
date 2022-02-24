ce5e1287ba65fc12807e8410de2487bc4b8e0b6d
ByWaleed <waleedi825@gmail.com>
2022-02-24 19:44:43 +0000

    Find or Inject Web3 Provider

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,25 @@ App = {
     init: async function () {
         return await App.initWeb3();
     },
+
+    initWeb3: async function () {
+        // Find or Inject Web3 Provider
+        if (window.ethereum) {
+            App.web3Provider = window.ethereum;
+            try {
+                await window.ethereum.enable(); // Request account access
+            } catch (error) {
+                // User denied account access...
+                console.error("User denied account access");
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3)
+            App.web3Provider = window.web3.currentProvider;
+        // If no injected web3 instance is detected, fall back to Ganache
+        else
+            App.web3Provider = new Web3.providers.HttpProvider("http://localhost:8545");
+    }
 };
 
 $(function () {
