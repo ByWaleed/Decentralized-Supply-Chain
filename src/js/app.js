99b8f3683393d55b37b4b0150c20a5c3dd567642
ByWaleed <waleedi825@gmail.com>
2022-02-24 19:48:39 +0000

    Get waller account accress function implemented

--- src/js/app.js
+++ src/js/app.js
@@ -15,11 +15,28 @@ App = {
             }
         }
         // Legacy dapp browsers...
-        else if (window.web3)
+        else if (window.web3) {
             App.web3Provider = window.web3.currentProvider;
-        // If no injected web3 instance is detected, fall back to Ganache
-        else
+        } // If no injected web3 instance is detected, fall back to Ganache
+        else {
             App.web3Provider = new Web3.providers.HttpProvider("http://localhost:8545");
+        }
+
+        App.getMetaskAccountID();
+    },
+
+    getMetaskAccountID: function () {
+        web3 = new Web3(App.web3Provider);
+
+        // Retrieving accounts
+        web3.eth.getAccounts(function (err, res) {
+            if (err) {
+                console.log("Error:", err);
+                return;
+            }
+            console.log("getMetaskID:", res);
+            App.metamaskAccountID = res[0];
+        });
     }
 };
 
