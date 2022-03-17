d2fb359c3b14cba085450f6fea8c4dc3aaee739d
ByWaleed <waleedi825@gmail.com>
2022-03-17 14:35:47 +0000

    Contracts migration recreated

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,14 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+// migrating the appropriate contracts
+var Manufacturer = artifacts.require("./Manufacturer.sol");
+var Distributor = artifacts.require("./Distributor.sol");
+var Retailer = artifacts.require("./Retailer.sol");
+var Consumer = artifacts.require("./Consumer.sol");
+var SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+  deployer.deploy(Manufacturer);
+  deployer.deploy(Distributor);
+  deployer.deploy(Retailer);
+  deployer.deploy(Consumer);
+  deployer.deploy(SupplyChain);
 };
