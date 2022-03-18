89602d3cbde02043a3fe29d3c7467585e6a4bb0d
ByWaleed <waleedi825@gmail.com>
2022-03-18 10:08:36 +0000

    Imported and setup drizzle instance

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ import Items from "./components/items/Items";
 import Users from './components/users/Users';
 import Search from './components/search/Search';
 
-// const drizzle = new Drizzle(drizzleOptions);
+const drizzle = new Drizzle(drizzleOptions);
 
 const App = () => {
     return (
